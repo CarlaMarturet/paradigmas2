aa4c6b484995cb8297253c7d5ddccfd1bc30ea33
carla <abrevadero.4020@gmail.com>
2026-08-12 17:17:17 -0300

    segundo commit

--- segundaBranch.java
+++ segundaBranch.java
@@ -2,4 +2,6 @@ public class segundaBranch {
     public static void main(String[] args) {
         System.out.println("Hola sergunda rama");
     }
+
+
 }
---
+++ segundocommit.java
@@ -0,0 +1,9 @@
+public class segundocommit {
+    public class segundaBranch {
+    public static void main(String[] args) {
+        System.out.println("este es mi segundo commit en la segunda rama");
+    }
+
+
+}
+}
