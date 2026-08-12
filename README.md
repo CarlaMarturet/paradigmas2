c666980d5284cc1fe2845a7e8882e553303ca293
carla <abrevadero.4020@gmail.com>
2026-08-12 16:52:46 -0300

    cambios en el readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # paradigmas2
+# esto es un documento para realizar la actividad de subir commits  a un repo
--- iniciorepo.java
+++ iniciorepo.java
@@ -1,4 +1,4 @@
-//hola mundo
+// primer hola mundo
 public class iniciorepo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
